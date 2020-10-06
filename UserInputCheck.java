bcc756e70a62642947bbf4340263cbb42169b4e6
sfrewen <sfrewen@flintridgeprep.org>
2020-10-06 13:21:49 -0700

    Create UserInputCheck.java

---
+++ UserInputCheck.java
@@ -0,0 +1,7 @@
+class UserInputCheck {
+  public static void main(String[] args) {
+    //check all of the input arguments (using the args variable) to see if they match your first name. 
+    // If any of the arguments match, the program should print out a sentence saying hello.
+  }
+}
+    
