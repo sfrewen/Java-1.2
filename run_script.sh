fac38830493e6d3e74db154997a58b3551ebce11
sfrewen <sfrewen@flintridgeprep.org>
2020-10-06 13:19:41 -0700

    Compiles the right files and runs both programs

--- run_script.sh
+++ run_script.sh
@@ -1,2 +1,5 @@
-javac HelloWorldApp.java
-java HelloWorldApp
+javac UserInputCheck.java
+javac Shuffle1.java
+java UserInputCheck Clarice Nathan Joey David
+java UserInputCheck Nobody Noname
+java Shuffle1
