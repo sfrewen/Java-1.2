7964c9fc8cc19b65c9dc97b692123b459fe3e3e6
sfrewen <sfrewen@flintridgeprep.org>
2020-10-06 13:23:29 -0700

    Create Shuffle1.java

---
+++ Shuffle1.java
@@ -0,0 +1,6 @@
+//Arrange the code in the pdf correctly to get the main method to print "a-b c-d"
+//The first chunk is already placed.
+class Shuffle1 {
+  public static void main(String[] args) {
+  }
+}
